28e71b77490194ac9b752e05cde0e4ac080b4878
lukiepookieee <luke.weidner4@gmail.com>
2025-05-01 18:41:57 -0700

    lasers!!!

--- Scripts/GridLaser.gd
+++ Scripts/GridLaser.gd
@@ -1,28 +1,31 @@
 extends GridActor
 class_name GridLaser
+
+
+
 # float for the time in between positions
 var travel_time: float = 0.5
+@onready var move_timer = $moveTimer
 signal collision
 
-func _process(delta: float) -> void:
-	var nextPosition := position
-	await get_tree().create_timer(0.5).timeout
-	position.z -= 1
-	if (not nextPosition.is_equal_approx(position)):
-		#see if we can move to that spot
-		var slots = g.getOccupants(nextPosition);
-		#if it's allowed
-		if (not slots is bool || slots != false):
-			#check if that wasn't a blank space
-			for slot in slots:
-				collision.emit(slot);
-			
-			#actually move
-			g.moveActor(self, nextPosition);
-			# moved.emit(position)
-			
-			#kind of drag the visual a bit
-			var tween = get_tree().create_tween()
-			tween.tween_property(
-				meshInstance, "global_position", g.getDrawPosition(position), 0.1
-			).set_ease(Tween.EASE_OUT);
+func _ready() -> void:
+	move_timer.start()
+	super();
+
+func beat() -> void:
+	pass
+	
+	
+func new_position():
+	var newPos := position;
+	newPos.z -= 1;
+	tryMoving(newPos);	
+	moveVisual();
+	
+	
+	
+
+
+
+func _on_move_timer_timeout() -> void:
+	new_position()
--- Scripts/GridPlayer.gd
+++ Scripts/GridPlayer.gd
@@ -5,6 +5,9 @@ signal moved(Vector3)
 signal collision(GridActor)
 @export var health: int = 2
 @onready var audio = $AudioStreamPlayer
+@onready var shot_marker = $Visual/shotMarker
+
+const LASER = preload("res://Scenes/GridActors/grid_laser.tscn")
 
 func _ready() -> void:
 	position = Vector3(floor(g.size.x/2), floor(g.size.y/2), g.size.z-1);
@@ -25,6 +28,8 @@ func _process(delta: float) -> void:
 	elif (Input.is_action_just_pressed("ui_down")):
 		nextPosition.y -= 1;
 	
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot_laser()
 	
 	#if we moved
 	if (not nextPosition.is_equal_approx(position)):
@@ -39,12 +44,21 @@ func _process(delta: float) -> void:
 			#actually move
 			g.moveActor(self,nextPosition);
 			moved.emit(position)
+			print(position)
+			shot_marker.position = position
+			
+			
 			
 			#kind of drag the visual a bit
 			var tween = get_tree().create_tween()
 			tween.tween_property(
 				meshInstance, "global_position", g.getDrawPosition(position), 0.1
 			).set_ease(Tween.EASE_OUT);
+
+func shoot_laser():
+	g.spawnGridActor(LASER, shot_marker.position)
+	#$Bullets.add_child(new_laser)
+	
 
 func collide(other: GridActor) -> void:
 	#if anything that's not harmful gets added to the game we should handle it here
